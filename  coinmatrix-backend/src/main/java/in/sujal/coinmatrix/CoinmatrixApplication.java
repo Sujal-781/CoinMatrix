462ae0cf6fa8f86d6416417d08e66566478f4758
Sujal Choudhary <chsujal8@gmail.com>
2025-10-24 12:28:05 +0530

    Created the first api unser HomeController

---
+++  coinmatrix-backend/src/main/java/in/sujal/coinmatrix/CoinmatrixApplication.java
@@ -0,0 +1,13 @@
+package in.sujal.coinmatrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinmatrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinmatrixApplication.class, args);
+	}
+
+}
---
+++  coinmatrix-backend/src/test/java/in/sujal/coinmatrix/CoinmatrixApplicationTests.java
@@ -0,0 +1,13 @@
+package in.sujal.coinmatrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinmatrixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
