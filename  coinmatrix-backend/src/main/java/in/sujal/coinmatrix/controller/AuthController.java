39cbc246935b7755601611454edef09e6865ee32
Sujal Choudhary <chsujal8@gmail.com>
2025-10-24 13:23:17 +0530

    added jwt dependencies which will be used for security filtering

---  coinmatrix-backend/src/main/java/in/sujal/coinmatrix/controller/AuthController.java
+++  coinmatrix-backend/src/main/java/in/sujal/coinmatrix/controller/AuthController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,14 +17,14 @@ public class AuthController {
     private UserRepository userRepository;
 
     @PostMapping("/signup")
-    public ResponseEntity<User> register(User user){
+    public ResponseEntity<User> register(@RequestBody User user){
 
 
         User newUser = new User();
         newUser.setEmail(user.getEmail());
         newUser.setPassword(user.getPassword());
         newUser.setEmail(user.getEmail());
-
+        newUser.setFullname(user.getFullname());
         User savedUser = userRepository.save(newUser);
 
         return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
